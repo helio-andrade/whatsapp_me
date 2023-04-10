daad9e99dfeff3bf952401dfa6b0addfb4e6170a
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-04-10 00:11:35 -0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'wa_me.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,22 +6,63 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WhatsApp Link Generator',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
+      title: 'Formulário de Telefone',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('WhatsApp Link Generator'),
+          title: Text('Formulário de Telefone'),
         ),
-        body: Center(
-          child: Container(
-            constraints: BoxConstraints(maxWidth: 600),
-            padding: EdgeInsets.all(16),
-            child: FormWhatsapp(),
+        body: PhoneNumberForm(),
+      ),
+    );
+  }
+}
+
+class PhoneNumberForm extends StatefulWidget {
+  @override
+  _PhoneNumberFormState createState() => _PhoneNumberFormState();
+}
+
+class _PhoneNumberFormState extends State<PhoneNumberForm> {
+  final _formKey = GlobalKey<FormState>();
+  late String _phoneNumber;
+
+  void _submitPhoneNumber() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      // Aqui você pode fazer algo com o número de telefone inserido
+      print('Número de telefone: $_phoneNumber');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.phone,
+            decoration: InputDecoration(
+              hintText: 'Digite o número de telefone',
+              border: OutlineInputBorder(),
+            ),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Por favor, insira um número de telefone';
+              }
+              return null;
+            },
+            onSaved: (value) {
+              _phoneNumber = value!;
+            },
           ),
-        ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _submitPhoneNumber,
+            child: Text('Enviar'),
+          ),
+        ],
       ),
     );
   }
